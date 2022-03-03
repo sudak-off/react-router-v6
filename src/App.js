df8c01af6d9060824170998fc21b7c8e168f3b6a
Tema <krestonos@gmail.com>
2022-03-03 03:49:58 +0200

    базовые настройки

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link } from "react-router-dom";
+
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import BlogPage from './pages/BlogPage'
+import NotFoundPage from './pages/NotFoundPage'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <Link to="/">Home</Link>
+            <Link to="/blog">Post</Link>
+            <Link to="/about">About</Link>
+        </header>
+        {/*<div>*/}
+        {/*    <h1>Get started with React-Router v6</h1>*/}
+        {/*</div>*/}
+        <Routes>
+            <Route path='/' element={<HomePage />}/>
+            <Route path='/about' element={<AboutPage />}/>
+            <Route path='/blog' element={<BlogPage />}/>
+            <Route path='*' element={<NotFoundPage />}/>
+        </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +18,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import {BrowserRouter} from "react-router-dom";
+// обернули наше приложение
